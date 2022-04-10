2f79de065ab461227efb829da15140d28d77ea7c
Max Schafer <schaferm5@mymacewan.ca>
2022-04-10 16:27:50 -0600

    init

---
+++ loadTexture.js
@@ -0,0 +1,8 @@
+
+
+function loadTexture(){
+    //document.getElementById("crate-image").src = document.getElementById("myFile").files[0].name;
+    console.log( document.getElementById("myFile").files[0]);
+    InitDemo();
+    
+}
